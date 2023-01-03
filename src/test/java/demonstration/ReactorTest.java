fa8c4dbcb84ae0bfb6d1097aee02b0e2785bdf8c
akenz1901 <michael_aka1@icloud.com>
2023-01-03 21:16:40 +0100

    Project Reactor Practices

---
+++ src/test/java/demonstration/ReactorTest.java
@@ -0,0 +1,359 @@
+package demonstration;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ReactorTest {
+
+    @Test
+    public void createFluxWithJust(){
+        Flux<String> fruitFlux = Flux
+                .just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+        fruitFlux.subscribe(f -> System.out.println("Here's some fruit " + f));
+
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createFluxFromArray(){
+        String[] fruitArr = {"Apple", "Orange", "Grape", "Banana", "Strawberry"};
+
+        Flux<String> fruitFlux = Flux.fromArray(fruitArr);
+
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createFluxFromIterable(){
+        List<String> fruitIterable = new ArrayList<>();
+        fruitIterable.add("Apple");
+        fruitIterable.add("Orange");
+        fruitIterable.add("Grape");
+        fruitIterable.add("Banana");
+        fruitIterable.add("Strawberry");
+
+        Flux<String> fruitFlux = Flux.fromIterable(fruitIterable);
+
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createFluxFromStream(){
+        Stream<String> fruitStream = Stream.of("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
+        Flux<String> fruitFlux = Flux.fromStream(fruitStream);
+
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createFluxFromRangeOfNumber(){
+        Flux<Integer> intervalFlux =
+                Flux.range(1, 5);
+
+        StepVerifier.create(intervalFlux)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .expectNext(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void createFluxFromAnInterval(){
+        Flux<Long> intervalFlux =
+                Flux.interval(Duration.ofSeconds(1))
+                .take(5);
+
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mergeFluxes(){
+        Flux<String> characterFlux =
+                Flux.just("Garfield", "Kojak", "Barbossa")
+                .delayElements(Duration.ofMillis(500));
+
+        Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples")
+                .delaySubscription(Duration.ofMillis(250))
+                .delayElements(Duration.ofMillis(500));
+
+        Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+
+        StepVerifier.create(mergedFlux)
+                .expectNext("Garfield")
+                .expectNext("Lasagna")
+                .expectNext("Kojak")
+                .expectNext("Lollipops")
+                .expectNext("Barbossa")
+                .expectNext("Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxes(){
+        Flux<String> characterFlux =
+                Flux.just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        Flux<Tuple2<String, String>> zippedFlux =
+                Flux.zip(characterFlux, foodFlux);
+
+        StepVerifier.create(zippedFlux)
+                .expectNextMatches(
+                        p -> p.getT1().equals("Garfield") &&
+                             p.getT2().equals("Lasagna")
+                )
+                .expectNextMatches(
+                        p -> p.getT1().equals("Kojak") &&
+                                p.getT2().equals("Lollipops")
+                )
+                .expectNextMatches(
+                        p -> p.getT1().equals("Barbossa") &&
+                                p.getT2().equals("Apples")
+                )
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void zipFluxesToObject(){
+        Flux<String> characterFlux =
+                Flux.just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        Flux<String> zippedFlux =
+                Flux.zip(characterFlux, foodFlux, (c,f) -> c + " eats " + f);
+
+        StepVerifier.create(zippedFlux)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojask eats Lollipops")
+                .expectNext("Barbossa eats Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void firstWithSignalFlux(){
+        Flux<String> slowFlux =
+                Flux.just("tortoise", "snail", "slot")
+                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> fastFlux = Flux.just("hare", "cheetah", "squirrel");
+
+        Flux<String> firstFlux = Flux.firstWithSignal(slowFlux, fastFlux);
+
+        StepVerifier.create(firstFlux)
+                .expectNext("hare")
+                .expectNext("cheetah")
+                .expectNext("squirrel")
+                .verifyComplete();
+    }
+
+    @Test
+    public void skipAFew(){
+        Flux<String> countFlux =
+                Flux.just("one", "two", "skip a few", "ninety nine", "one hundred")
+                .skip(3);
+
+        StepVerifier.create(countFlux)
+                .expectNext("ninety nine", "one hundred")
+                .verifyComplete();
+    }
+
+    @Test
+    public void skipAFewSeconds(){
+        Flux<String> countFlux = Flux.just(
+                "one", "two", "skip a few", "ninety nine", "one hundred"
+        ).delayElements(Duration.ofSeconds(1))
+                .skip(Duration.ofSeconds(4));
+        StepVerifier.create(countFlux)
+                .expectNext("ninety nine", "one hundred")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void take() {
+        Flux<String> nationalParkFlux = Flux.just(
+                "Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Acadia")
+                .take(3);
+        StepVerifier.create(nationalParkFlux)
+                .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
+                .verifyComplete();
+    }
+
+    @Test
+    public void takeForAwhile(){
+        Flux<String> nationalParkFlux = Flux.just(
+                "Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+                .delayElements(Duration.ofSeconds(1))
+                .take(Duration.ofMillis(3500));
+        StepVerifier.create(nationalParkFlux)
+                .expectNext("Yellowstone", "Yosemite", "Grand Canyon")
+                .verifyComplete();
+    }
+
+    @Test
+    public void filter(){
+        Flux<String> nationalParkFlux = Flux.just(
+                "Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+                .filter(np -> !np.contains(" "));
+
+        StepVerifier.create(nationalParkFlux)
+                .expectNext("Yellowstone", "Yosemite", "Zion")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void distinct() {
+        Flux<String> animalFlux = Flux.just(
+                "dog", "cat", "bird", "dog", "bird", "anteater")
+                .distinct();
+        StepVerifier.create(animalFlux)
+                .expectNext("dog", "cat", "bird", "anteater")
+                .verifyComplete();
+    }
+
+    @Test
+    public void buffer() {
+        Flux<String> fruitFlux = Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry");
+
+        Flux<List<String>> bufferedFlux = fruitFlux.buffer(3);
+
+        StepVerifier
+                .create(bufferedFlux)
+                .expectNext(Arrays.asList("apple", "orange", "banana"))
+                .expectNext(Arrays.asList("kiwi", "strawberry"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void bufferAndFlatMap() throws Exception {
+        Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry")
+                .buffer(3)
+                .flatMap(x ->
+                        Flux.fromIterable(x)
+                                .map(String::toUpperCase)
+                                .subscribeOn(Schedulers.parallel())
+                                .log()
+                ).subscribe();
+    }
+
+    @Test
+    public void collectList() {
+        Flux<String> fruitFlux = Flux.just(
+                "apple", "orange", "banana", "kiwi", "strawberry");
+
+        Mono<List<String>> fruitListMono = fruitFlux.collectList();
+
+        StepVerifier
+                .create(fruitListMono)
+                .expectNext(Arrays.asList(
+                        "apple", "orange", "banana", "kiwi", "strawberry"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void collectMap() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+        Mono<Map<Character, String>> animalMapMono =
+                animalFlux.collectMap(a -> a.charAt(0));
+
+        StepVerifier
+                .create(animalMapMono)
+                .expectNextMatches(map -> map.size() == 3 &&
+                        map.get('a').equals("aardvark") &&
+                        map.get('e').equals("eagle") &&
+                        map.get('k').equals("kangaroo"))
+                .verifyComplete();
+    }
+
+    /**
+     * Suppose you want to know that every String published by a Flux contains the letter a
+     * or the letter k. The following test shows how to use all() to check for that condition:
+     */
+
+    @Test
+    public void all() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+        Mono<Boolean> hasAMono = animalFlux.all(a -> a.contains("a"));
+        StepVerifier.create(hasAMono)
+                .expectNext(true)
+                .verifyComplete();
+
+        Mono<Boolean> hasKMono = animalFlux.all(a -> a.contains("k"));
+        StepVerifier.create(hasKMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+    @Test
+    public void any() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+
+        Mono<Boolean> hasTMono = animalFlux.any(a -> a.contains("t"));
+
+        StepVerifier.create(hasTMono)
+                .expectNext(true)
+                .verifyComplete();
+
+        Mono<Boolean> hasZMono = animalFlux.any(a -> a.contains("z"));
+        StepVerifier.create(hasZMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+}
